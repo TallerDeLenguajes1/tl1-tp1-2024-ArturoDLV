91318dec0fe2ae7d36606a3e1413353a593635f2
Abregu Marcelo <marceloooabregu015@gmail.com>
2024-03-26 13:50:31 -0300

    Inciso e de punteros hecho

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,7 +2,15 @@
 
 int main()
 {
-    printf("Hola mundo");
+    float pi = 3.14159265358979327;
+    float *puntero = &pi;
+
+    printf("\nHola mundo.\n");
+    printf("\nContenido del puntero: %f",*puntero);
+    printf("\nDireccion guardada por el puntero: %p",puntero);
+    printf("\nDireccion de la variable: %p",&pi);
+    printf("\nDireccion del puntero: %p",&puntero);
+    printf("\nTamaño ocupado por el contenido del puntero: %d",sizeof(*puntero));
 
     return 0;
 }
